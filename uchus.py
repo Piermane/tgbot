742be8b5d6b0efbc1853b9179ef4d26ed9367f78
Балашов Виталий <balashov.vitaliy2@wildberries.work>
2024-06-26 18:12:05 +0300

    55555

--- uchus.py
+++ uchus.py
@@ -52,17 +52,16 @@ async def cancel_current_state(message: types.Message, state: FSMContext):
 
     # Выполнение соответствующей команды после завершения состояния
     if message.text == "Задать вопрос спикеру":
-        await ask_speaker(message, state)
+        await ask_speaker(message)
     elif message.text == "Задать вопрос помощнику":
-        await ask_ai(message, state)
+        await ask_ai(message)
     elif message.text == "Генерировать черно-белое изображение":
-        await generate_image_prompt(message, state)
+        await generate_image_prompt(message)
     elif message.text == "Играть в игру":
-        await play_game(message, state)
+        await play_game(message)
 
 @dp.message_handler(lambda message: message.text == "Задать вопрос спикеру")
 async def ask_speaker(message: types.Message, state: FSMContext):
-    await state.finish()  # Завершаем текущее состояние
     logger.info(f"Получена команда 'Задать вопрос спикеру' от {message.from_user.id}")
     await message.reply("Выберите зал, в котором вы находитесь:", reply_markup=hall_keyboard)
     await BotStates.WAITING_FOR_HALL.set()
@@ -108,7 +107,6 @@ async def send_question_to_django(message: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda message: message.text == "Задать вопрос помощнику")
 async def ask_ai(message: types.Message, state: FSMContext):
-    await state.finish()  # Завершаем текущее состояние
     logger.info(f"Получена команда 'Задать вопрос помощнику' от {message.from_user.id}")
     await message.reply("Введите свой вопрос для помощника (ИИ)", reply_markup=ReplyKeyboardRemove())
     await BotStates.WAITING_FOR_AI_QUESTION.set()
@@ -153,7 +151,6 @@ async def handle_ai_response(message: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda message: message.text == "Генерировать черно-белое изображение")
 async def generate_image_prompt(message: types.Message, state: FSMContext):
-    await state.finish()  # Завершаем текущее состояние
     logger.info(f"Получена команда 'Генерировать черно-белое изображение' от {message.from_user.id}")
     await message.reply("Введите описание для генерации черно-белого изображения", reply_markup=ReplyKeyboardRemove())
     await BotStates.WAITING_FOR_IMAGE_PROMPT.set()
@@ -197,7 +194,6 @@ async def handle_image_generation(message: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda message: message.text == "Играть в игру")
 async def play_game(message: types.Message, state: FSMContext):
-    await state.finish()  # Завершаем текущее состояние
     logger.info(f"Получена команда 'Играть в игру' от {message.from_user.id}")
     keyboard = InlineKeyboardMarkup()
     keyboard.add(InlineKeyboardButton("Играть в Skipper", url=GAME_URL))
